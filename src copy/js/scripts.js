385deea7fd04578264789b9704dcd5fc7931557b
Victor-Ezekiel-Blinkz <victoruche3022@gmail.com>
2023-12-05 08:39:27 +0100

    Agate update

---
+++ src copy/js/scripts.js
@@ -0,0 +1,28 @@
+var TrandingSlider = new Swiper(".hero-container", {
+  // effect: "coverflow",
+  // grabCursor: true,
+  centeredSlides: true,
+  // loop: true,
+
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
